6e8c464c07ecc386fb6ab269733b044231e8c55a
Timur Shykhsefiyeu <nut.t@icloud.com>
2024-02-28 14:31:41 +0100

    fix: cross origin global config

--- src/main/java/pl/goeuropa/servicealerts/config/WebConfig.java
+++ src/main/java/pl/goeuropa/servicealerts/config/WebConfig.java
@@ -5,12 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//@EnableWebMvc
+@Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**");
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://alerts.goeuropa.net")
+                .allowedOrigins("http://localhost:3000");
+    }
 }
